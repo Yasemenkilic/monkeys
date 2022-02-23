10b2140109b85ad34259e2059a4d285845d0282a
Yasemen Kılıç <ysmn6667@gmail.com>
2022-02-23 22:22:51 +0300

    sdf

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gozukapali = document.querySelector('.kapali');
 const gozuacik = document.querySelector('.acik');
 
 gozukapali.addEventListener('click', () => {
-    if (gozuacik.classList.constains('acik')) {
+    if (gozuacik.classList.contains('acik')) {
         gozuacik.classList.add('active');
         gozukapali.classList.remove('active');
     }
