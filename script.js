d912629cc1c900005803ec5a0af2628cd70fc5bc
Yasemen Kılıç <ysmn6667@gmail.com>
2022-02-23 22:16:44 +0300

    ghjghj

---
+++ script.js
@@ -0,0 +1,16 @@
+const gozukapali = document.querySelector('.kapali');
+const gozuacik = document.querySelector('.acik');
+
+gozukapali.addEventListener('click', () => {
+    if (gozuacik.classList.constains('acik')) {
+        gozuacik.classList.add('active');
+        gozukapali.classList.remove('active');
+    }
+}  );
+
+gozuacik.addEventListener('click', () => {
+    if (gozukapali.classList.contains('kapali')) {
+        gozukapali.classList.add('active');
+        gozuacik.classList.remove('active');
+    }
+} );
